02c375365f9c9b424826bc614303d7f7ec94acc9
phucsun <ngtphuc0@gmail.com>
2024-11-22 08:38:41 +0700

    setting pug

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 9999;
 
+app.set("views", "./views");
+app.set("view engine" ,"pug");
+
+app.use(express.static('public'));
+
 app.get("/", (req, res) => {
-    res.send("ok");
+    res.send("Trang chủ");
 });
 
+app.get("/products", (req, res) => {
+    res.send("Trang sản phẩm")
+});
+  
 app.listen(port, () => {
-    console.log(`app are listening on port ${port}`);
+console.log(`app listening on port ${port}`)
 })
