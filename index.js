881037d33bf80a40d7a59011bacb23f735c408ea
phucsun <ngtphuc0@gmail.com>
2024-11-22 10:40:56 +0700

    module controller _MVC

--- index.js
+++ index.js
@@ -6,13 +6,9 @@ app.set("views", "./views");
 app.set("view engine", "pug");
 app.use(express.static('public'));
 
-// app.get("/", (req, res) => {
-//     res.send("Trang chủ");
-// });
 
 const clientRoutes = require("./routers/client/index.router.js");
 clientRoutes(app);
-// app.use("/products", clientRoutes);
 
 app.listen(port, () => {
     console.log(`app listening on port ${port}`)
--- routers/client/home.router.js
+++ routers/client/home.router.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const controller = require("../../controller/client/home.controller.js");
 
-router.get("/", (req, res) => {
-    res.send("Trang chủ");
-});
+router.get("/", controller.index);
 
 module.exports = router;
