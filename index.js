00dc1159a84f0b90a046d7cb5bf05e5eb061b70e
phucsun <ngtphuc0@gmail.com>
2024-11-23 15:11:05 +0700

    fix .env error

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
 const port = process.env.PORT;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
 app.use(express.static('public'));
-require('dotenv').config();
+
 
 
 const clientRoutes = require("./routers/client/index.router.js");
